d7f0a37e223b06f53041ecc63e1c1af89bc10cef
Joshua Tyree <joshuat@createthebridge.com>
2022-04-01 04:13:11 -0500

    Updated

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const listenPort = process.env.LISTEN_PORT || 3000
 fastify.post('/', async (req, resp) => {
   console.log(req.body);
   const postUrl = `https://${userName}:${password}@hoc.es.us-west-1.aws.found.io:9243/craft-stats/_doc`;
-  axios.post(postUrl, req.body)
-  return { status: "OK" }
+  const res = await axios.post(postUrl, req.body)
+  return res.data;
 })
 
 const start = async () => {
